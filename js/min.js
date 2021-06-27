f47098b16698c0167869fbcb9e23a7d39832ed6d
Serraji Mohssine <sera.mohssine@gmail.com>
2021-06-28 00:41:31 +0200

    add costum text and Footer, Contactus, Working hours, Aditional informations

--- js/min.js
+++ js/min.js
@@ -2,6 +2,16 @@ let menu = document.querySelector("#menu-bar");
 let navber = document.querySelector(".navber");
 let scrollTop = document.querySelector("#scroll-top");
 
+const userAction = async () => {
+  const response = await fetch("http://worldtimeapi.org/api/timezone/Europe/Amsterdam.json");
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+  //myJson = JSON.parse(myJson);
+  const day_of_week = myJson.day_of_week
+  var element = document.getElementById(day_of_week.toString());
+  element.classList.add("active_day");
+}
+
 menu.onclick = () =>{
   menu.classList.toggle("fa-times");
   navber.classList.toggle("active")
@@ -23,6 +33,7 @@ function loader () {
   document.querySelector(".loader-container").classList.add("fade-out")
 }
 function fadeOut(){
-  setInterval(loader , 3000)
+  setInterval(loader , 3000);
+  userAction()
 }
 window.onload = fadeOut ();
